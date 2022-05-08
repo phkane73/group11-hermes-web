c40847b36e0f8e36269f31e58fc17561cd105985
phkane73 <phkane732002@gmail.com>
2022-05-08 12:09:49 +0700

    update file

--- trangchu/starter.js
+++ trangchu/starter.js
@@ -1,54 +1,16 @@
 window.addEventListener("load", function () {
-  const slider = document.querySelector(".slider");
-  const sliderMain = document.querySelector(".slider-main");
-  const sliderItems = document.querySelectorAll(".slider-item");
-  const nextBtn = document.querySelector(".slider-next");
-  const prevBtn = document.querySelector(".slider-prev");
-  const dotItems = document.querySelectorAll(".slider-dot-item");
-  const sliderItemWidth = sliderItems[0].offsetWidth;
-  const sliderLength = sliderItems.length;
-  console.log("sliderItemWidth", sliderItemWidth);
-  let positionX = 0;
-  let index = 0;
-  nextBtn.addEventListener("click", function () {
-    handleChangeSlider(1);
-  });
-  prevBtn.addEventListener("click", function () {
-    handleChangeSlider(-1);
-  });
-
-  [...dotItems].forEach((item) =>
-    item.addEventListener("click", function (e) {
-      [...dotItems].forEach((el) => el.classList.remove("active"));
-      e.target.classList.add("active");
-      const sliderIndex = parseInt(e.target.dataset.index);
-      index = sliderIndex;
-      positionX = -1 * index * sliderItemWidth;
-      sliderMain.style = `transform: translateX(${positionX}px)`;
-    })
-  );
-
-  function handleChangeSlider(direction) {
-    if (direction == 1) {
-      if (index >= sliderLength - 1) {
-        index = sliderLength - 1;
-        return;
-      }
-      positionX = positionX - sliderItemWidth;
-      sliderMain.style = `transform: translateX(${positionX}px)`;
-      index++;
-    } else if (direction == -1) {
-      if (index <= 0) {
-        index = 0;
-        return;
-      }
-      positionX = positionX + sliderItemWidth;
-      sliderMain.style = `transform: translateX(${positionX}px)`;
-      index--;
+  var count = 1;
+  const colordef = document.getElementById("first-btn");
+  colordef.classList.add("colordef");
+  this.setInterval(function () {
+    const colorde = document.getElementById("btn" + count);
+    document.getElementById("radio" + count).checked = true;
+    count++;
+    colordef.classList.remove("colordef");
+    if (count > 5) {
+      count = 1;
     }
-    [...dotItems].forEach((el) => el.classList.remove("active"));
-    dotItems[index].classList.add("active");
-  }
+  }, 2500);
 });
 
 window.addEventListener("load", function () {
