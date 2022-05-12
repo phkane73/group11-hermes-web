1e762273543d468f80616dc58322643fff7aca1a
phkane73 <phkane732002@gmail.com>
2022-05-12 23:15:40 +0700

    updatefile

--- trangchu/starter.js
+++ trangchu/starter.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function () {
   var count = 1;
   const colordef = document.getElementById("first-btn");
+  console.log(document.getElementById("first-btn"));
   colordef.classList.add("colordef");
   this.setInterval(function () {
     const colorde = document.getElementById("btn" + count);
@@ -18,8 +19,8 @@ window.addEventListener("load", function () {
   const eye1 = document.querySelector(".eye1");
   eye.addEventListener("click", function () {
     eye.children[0].classList.toggle("fa-eye-slash");
-    const input = document.getElementsByTagName("input")[1];
-    console.log(input.attributes.type.value);
+    const input = document.getElementsByTagName("input")[2];
+    console.log(document.getElementsByTagName("input"));
     if (input.attributes.type.value == "text") {
       input.setAttribute("type", "password");
     } else {
@@ -28,8 +29,7 @@ window.addEventListener("load", function () {
   });
   eye1.addEventListener("click", function () {
     eye1.children[0].classList.toggle("fa-eye-slash");
-    const input = document.getElementsByTagName("input")[2];
-    console.log(input.attributes.type.value);
+    const input = document.getElementsByTagName("input")[3];
     if (input.attributes.type.value == "text") {
       input.setAttribute("type", "password");
     } else {
@@ -42,5 +42,11 @@ window.addEventListener("load", function () {
   const submit = document.querySelector(".form-submit");
   submit.addEventListener("click", function () {
     alert("Bạn đã đăng nhập thành công");
+  });
+});
+window.addEventListener("load", function () {
+  const submit2 = document.querySelector(".form-submit2");
+  submit2.addEventListener("click", function () {
+    alert("Bạn đã đăng ký thành công");
   });
 });
